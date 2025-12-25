ab900423c0d89220f7a3f1f1dc9299082f578972
Andrew <andrewrpelton@outlook.com>
2025-12-24 22:57:02 -0800

    added hotkeys

--- server.py
+++ server.py
@@ -3,14 +3,19 @@ import pyautogui
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/togglePlayback?":
+        if self.path == "/togglePlayback?" or self.path == "/togglePlayback":
             pyautogui.press("playpause")
 
-        elif self.path == "/prevTab?":
+        elif self.path == "/prevTab?" or self.path == "/prevTab":
             pyautogui.hotkey("ctrl", "shift", "tab")
 
-        elif self.path == "/nextTab?":
+        elif self.path == "/nextTab?" or self.path == "/nextTab":
             pyautogui.hotkey("ctrl", "tab")
+
+        elif self.path == "/hotkey1?" or self.path == "/hotkey1":
+            pyautogui.hotkey("ctrl", "k")
+            pyautogui.typewrite("https://youtube.com\n")
+
 
         self.send_response(200)
         self.send_header("Contest-type", "text/html")
